f49742b0945019ef9857734398cdb5b57c88ac80
Jamielly Reis <100716307+Jamielly@users.noreply.github.com>
2024-01-18 20:31:16 -0300

    CRUD - JSON

--- SERIES/index.js
+++ SERIES/index.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+
 let seriesData = [
     { id: 1, nome: 'Breaking Bad', genero: 'Drama', temporadas: 5, anoLancamento: 2008, rating: 9.5 },
     { id: 2, nome: 'Stranger Things', genero: 'Ficção Científica', temporadas: 4, anoLancamento: 2016, rating: 8.7 },
